d45c5cd4fed511420e9ed25884850feb272f647d
viveknhc <viveknhc@gmail.com>
2022-08-29 18:16:31 +0530

    first commit

---
+++ account_security/settings.py
@@ -0,0 +1,131 @@
+
+import os
+from dotenv import load_dotenv
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '!@v)i0w+*@ld2tsr#l-e6-zp$a(k*%qt68+ago0i(yn6z9g35_'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'twofa',
+    'phone_verification',
+    'clients'
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'account_security.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'account_security.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.11/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+LOGIN_URL = '/login/'
+LOGOUT_URL = '/logout/'
+LOGIN_REDIRECT_URL = '/protected/'
+LOGOUT_REDIRECT_URL = '/login/'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+# Use our custom User Model
+AUTH_USER_MODEL = 'twofa.TwoFAUser'
+
+# Authy Application Key
+ACCOUNT_SECURITY_API_KEY = '7ogin/L88jqzSsLyS7TTSB1pAzoFnA3fumsqr'
+TWILIO_ACCOUNT_SID = 'AC37d08f67baf21e8687f5b5db7690f68b'
+TWILIO_AUTH_TOKEN = 'a1a58e3c48cdfbe194a8be02214bcc9a'
+TWILIO_VERIFICATION_SID = 'VA52317b482f7676cceeeb9c59e86043be'
---
+++ phone_verification/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+
+from clients import twilio_client
+from .forms import VerificationForm, TokenForm
+
+
+def phone_verification(request):
+    if request.method == 'POST':
+        form = VerificationForm(request.POST)
+        if form.is_valid():
+            request.session['phone_number'] = form.cleaned_data['phone_number']
+            verification = twilio_client.verifications(form.cleaned_data['phone_number'], form.cleaned_data['via'])
+            return redirect('token_validation')
+    else:
+        form = VerificationForm()
+    return render(request, 'phone_verification.html', {'form': form})
+
+
+def token_validation(request):
+    if request.method == 'POST':
+        form = TokenForm(request.POST)
+        if form.is_valid():
+            verification = twilio_client.verification_checks(request.session['phone_number'], form.cleaned_data['token'])
+
+            if verification.status == 'approved':
+                request.session['is_verified'] = True
+                return redirect('verified')
+            else:
+                for error_msg in verification.errors().values():
+                    form.add_error(None, error_msg)
+    else:
+        form = TokenForm()
+    return render(request, 'token_validation.html', {'form': form})
+
+
+def verified(request):
+    if not request.session.get('is_verified'):
+        return redirect('phone_verification')
+    return render(request, 'verified.html')
